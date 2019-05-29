1b4e2c8867992695685070a876e5be9555bca1eb
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-05-29 19:04:00 +0200

    Add project files.

---
+++ Shelter2.0/Models/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shelter.Models
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Advert> Adverts { get; set; }
+        public DbSet<IdentityUser> AspNetUsers { get; set; }
+    }
+}
